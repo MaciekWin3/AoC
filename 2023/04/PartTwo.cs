36360e4aa25b12d8e197c739bf6c42f4ff793310
maciekwin3 <maciekwin3@gmail.com>
2023-12-04 19:14:08 +0100

    Day 4 edition 2023 part 2 - done

--- 2023/04/PartTwo.cs
+++ 2023/04/PartTwo.cs
@@ -1,12 +1,17 @@
 ﻿namespace _04
 {
-    public class PartOne
+    public class PartTwo
     {
         private const string FILE_NAME = "input.txt";
+        public static Dictionary<int, int> CopiesOfWinningCupons = new();
         public static int Solve()
         {
             int score = 0;
             var input = GetPuzzleInputLines(FILE_NAME);
+            for (int i = 1; i <= input.Count; i++)
+            {
+                CopiesOfWinningCupons.Add(i, 1);
+            }
             foreach (var line in input)
             {
                 int localPoints = 0;
@@ -19,23 +24,24 @@
                         localPoints++;
                     }
                 }
-                if (localPoints != 0)
+                var copiesOfCupons = CopiesOfWinningCupons[game.Id];
+                for (int j = 0; j < copiesOfCupons; j++)
                 {
-                    score += CalculateScore(localPoints);
+                    for (int i = game.Id + 1; i < game.Id + localPoints + 1; i++)
+                    {
+                        if (CopiesOfWinningCupons.TryGetValue(i, out int value))
+                        {
+                            var x = value + 1;
+                            CopiesOfWinningCupons[i] = x;
+                        }
+                    }
                 }
-                Console.WriteLine($"{game.Id}: {CalculateScore(localPoints)}");
             }
-
-            return score;
-        }
-
-        public static int CalculateScore(int points)
-        {
-            int score = 1;
-            for (int i = 1; i < points; i++)
+            foreach (var item in CopiesOfWinningCupons)
             {
-                score *= 2;
+                score += item.Value;
             }
+
             return score;
         }
 
@@ -75,8 +81,8 @@
         public record Game
         {
             public int Id { get; set; }
-            public List<int> WinningNumbers {get; set;} = new List<int>();
-            public List<int> Numbers {get; set;} = new List<int>();
+            public List<int> WinningNumbers { get; set; } = new List<int>();
+            public List<int> Numbers { get; set; } = new List<int>();
         }
 
         private static List<string> GetPuzzleInputLines(string fileName)
--- 2023/04/Program.cs
+++ 2023/04/Program.cs
@@ -1,3 +1,4 @@
 ﻿using _04;
 
-Console.WriteLine(PartOne.Solve());
+//Console.WriteLine(PartOne.Solve());
+Console.WriteLine(PartTwo.Solve());
